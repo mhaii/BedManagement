607bbdc233d498068faa70a5fe21cee14c865ffd
mhaii <chess4G63>
2016-01-08 12:07:01 +0700

    add script to create new app

---
+++ scripts/create_app.py
@@ -0,0 +1,4 @@
+from scripts.script import create_app
+
+if __name__ == '__main__':
+    create_app()
--- scripts/script.py
+++ scripts/script.py
@@ -1,16 +1,21 @@
 import os
 import sys
 
-from os.path import join
-
 python = sys.executable
-pip = join(os.path.dirname(python), 'pip')
-rootdir = os.path.dirname(os.path.dirname(__file__))
+pip = os.path.join(os.path.dirname(python), 'pip')
+rootDir = os.path.dirname(os.path.dirname(__file__))
+manage = os.path.join(rootDir, 'manage.py')
+
+os.chdir(rootDir)
 
 
 def collect_static():
-    os.system('{0} {1} collectstatic --noinput -i *.sass -i *.coffee'.format(python, join(rootdir, 'manage.py')))
+    os.system('{0} {1} collectstatic --noinput -i *.sass -i *.coffee'.format(python, manage))
 
 
 def install_requirement():
-    os.system('{0} install -r {1}'.format(pip, join(rootdir, 'requirement.txt')))
+    os.system('{0} install -r {1}'.format(pip, os.path.join(rootDir, 'requirement.txt')))
+
+
+def create_app():
+    os.system('{0} {1} startapp {2}'.format(python, manage, input('New app name?: ')))
