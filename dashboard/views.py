1db8bbad3eb291a8b69f94fa6ad444cb11095a37
mhaii <bloo20xd@gmail.com>
2016-01-09 21:41:47 +0700

    modify landing redirection

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views.generic import View
@@ -7,14 +7,14 @@ from django.views.generic import View
 
 class Landing(LoginRequiredMixin, View):
     def get(self, request):
-        return render(request, 'basefile.html')
-
-
-class Status(View):
-    def get(self, request):
         return render(request, 'status-all.html')
 
 
-class CheckIn(View):
+class Status(LoginRequiredMixin, View):
+    def get(self, request):
+        return redirect('landing')
+
+
+class CheckIn(LoginRequiredMixin, View):
     def get(self, request):
         return render(request, 'check-in.html')
