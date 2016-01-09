8d3355302a3ea20620b59341a8fca8aca7af914b
mhaii <bloo20xd@gmail.com>
2016-01-09 23:52:42 +0700

    add TODO-remove

--- authentication/urls.py
+++ authentication/urls.py
@@ -19,5 +19,6 @@ from .views import *
 urlpatterns = [
     url(r'^login/', LoginPage.as_view(), name='login'),
     url(r'^logout/', LogoutPage.as_view(), name='logout'),
+    # TODO get rid of this when finished
     url(r'^create/(?P<username>\w+)/(?P<password>\w+)', CreateUser.as_view()),
 ]
