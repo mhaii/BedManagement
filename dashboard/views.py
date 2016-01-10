fe0e3e598b6677d0d562bc1ded14e6fdd3dcb19b
Boss <nutchanon.9@gmail.com>
2016-01-10 16:09:55 +0700

    fix landing page

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,14 +7,19 @@ from django.views.generic import View
 
 class Landing(LoginRequiredMixin, View):
     def get(self, request):
-        return render(request, 'status-all.html')
+        return render(request, 'home.html')
 
 
 class Status(LoginRequiredMixin, View):
     def get(self, request):
-        return redirect('landing')
+        return render(request, 'status-all.html')
 
 
 class CheckIn(LoginRequiredMixin, View):
     def get(self, request):
         return render(request, 'check-in.html')
+
+
+class Home(LoginRequiredMixin, View):
+    def get(self, request):
+        return redirect('landing')
