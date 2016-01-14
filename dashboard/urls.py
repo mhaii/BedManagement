1f05a841328bbe4af56489409bf9f57a3d712bff
Boss <nutchanon.9@gmail.com>
2016-01-14 14:47:48 +0700

    Create new example interface with data

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 
-from .views import CheckIn, CheckOut, Home, Landing, Status
+from .views import BookQueue, CheckOut, ChooseBed, Home, Landing, Status, EditPatientInfo, Queues
 
 urlpatterns = [
     url(r'^home/', Home.as_view(), name='home'),
     url(r'^status/$', Status.as_view(), name='status'),
-    url(r'^check-in/', CheckIn.as_view(), name='check_in'),
+    url(r'^book-queue/', BookQueue.as_view(), name='book_queue'),
+    url(r'^edit-patient-info-filled/', EditPatientInfo.as_view(), name='edit_patient_info_filled'),
     url(r'^check-out/', CheckOut.as_view(), name='check_out'),
+    url(r'^choose-bed/', ChooseBed.as_view(), name='choose_bed'),
+    url(r'^queues', Queues.as_view(), name='queues'),
     url(r'^$', Landing.as_view(), name='landing'),
 ]
