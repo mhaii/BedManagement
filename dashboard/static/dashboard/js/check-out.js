8bde8ac52938aa6527c4382cad8f79a80ec36ca5
Boss <nutchanon.9@gmail.com>
2016-01-15 16:03:09 +0700

    Create check-out.js for angular controller

--- dashboard/static/dashboard/js/check-out.js
+++ dashboard/static/dashboard/js/check-out.js
@@ -1,7 +1,28 @@
-var my_app = angular.module('MyApp',['ng.django.forms']);
-
-my_app.controller('myCtrl', function($scope) {
+var my_app = angular.module('MyApp',[]).config(function($interpolateProvider) {
+    $interpolateProvider.startSymbol('{$');
+    $interpolateProvider.endSymbol('$}');
+});
+var toggle = 0;
+my_app.controller('myCtrl', function($scope,$filter) {
+    var statusP = 0;
+    $scope.progStatus = "Should-Discharge Today";
     $scope.myfunc = function() {
-        $scope.count++
+        var date = new Date();
+
+        if(toggle==0) {
+            $scope.startTime = $filter('date')(new Date(), 'HH:mm:ss');
+            $scope.endTime = "";
+        }
+        $scope.startTime = $filter('date')(new Date(), 'HH:mm:ss');
+
+        statusP++;
+        switch(statusP) {
+                case 0:$scope.progStatus = "Should-Discharge Today";$scope.startTime = "";break;
+                case 1:$scope.progStatus = "Doctor Approved Discharge";break;
+                case 2:$scope.progStatus = "ออกใบนัด";break;
+                case 3:$scope.progStatus = "ส่งใบเบิกยา"; statusP = -1; break;
+        }
+
     }
-});ß
+
+});
