6f5fe14a5e654b46dbd1f1b83453b0615d431c10
mhaii <chess4G63>
2016-01-17 04:31:25 +0700

    add log functions

--- bedManagement/gunicorn.py
+++ bedManagement/gunicorn.py
@@ -7,6 +7,32 @@ loglevel = 'warning'
 bind = 'unix:///{0}'.format(join(dirname(dirname(__file__))), 'django.sock')
 workers = multiprocessing.cpu_count() * 2 + 1
 daemon = True
-threads = bind // 2
+threads = workers // 2
 user = 'daemon'
 group = 'daemon'
+
+proc_name = 'gUnicorn-bedmanagement'
+
+
+def post_fork(server, worker):
+    server.log.info("Worker spawned (pid: %s)", worker.pid)
+
+
+def pre_fork(server, worker):
+    pass
+
+
+def pre_exec(server):
+    server.log.info("Forked child, re-executing.")
+
+
+def when_ready(server):
+    server.log.info("Server is ready. Spawning workers")
+
+
+def worker_int(worker):
+    worker.log.info("worker received INT or QUIT signal")
+
+
+def worker_abort(worker):
+    worker.log.info("worker received SIGABRT signal")
