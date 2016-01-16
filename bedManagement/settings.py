8c6a120fd37a887b87b0b3f81b733e29746b1959
mhaii <chess4G63>
2016-01-16 17:50:51 +0700

    remove setting for media files

--- bedManagement/settings.py
+++ bedManagement/settings.py
@@ -125,9 +125,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 STATICFILES_FINDERS = ["django.contrib.staticfiles.finders.FileSystemFinder",
                        "django.contrib.staticfiles.finders.AppDirectoriesFinder",
                        "compressor.finders.CompressorFinder", ]
