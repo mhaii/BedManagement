6834f68e88f25f28499a2217bf4ccf710f929d6c
mhaii <chess4G63>
2016-01-17 01:35:03 +0700

    script for install fabric

---
+++ scripts/fabric.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+
+def fabric():
+    if sys.version_info[0] != 2:
+        print('This must be run with python 2 only!')
+        return
+    os.system('sudo easy_install pip')
+    os.system('pip install fabric')
+
+fabric()
