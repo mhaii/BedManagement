f0c9b4bd053ecf65afc013bbccec22d55a9420e6
mhaii <chess4G63>
2016-01-17 18:59:19 +0700

    try gunicorn manage script

--- bedManagement/gunicorn.py
+++ bedManagement/gunicorn.py
@@ -1,4 +1,6 @@
+#/usr/bin/python
 import multiprocessing
+import os
 from os.path import abspath, dirname, join
 
 dir = dirname(dirname(dirname(abspath(__file__))))
@@ -6,7 +8,7 @@ dir = dirname(dirname(dirname(abspath(__file__))))
 pidfile = join(dir, 'gunicorn.pid')
 errorlog = join(dir, 'gunicorn.log')
 loglevel = 'warning'
-bind = 'unix://{0}'.format(join(dir, 'gunicorn.sock'))
+bind = 'unix://%s' % join(dir, 'gunicorn.sock')
 workers = multiprocessing.cpu_count() * 2 + 1
 daemon = True
 threads = workers // 2
@@ -38,3 +40,50 @@ def worker_int(worker):
 
 def worker_abort(worker):
     worker.log.info("worker received SIGABRT signal")
+
+
+if __name__ == '__main__':
+    if os.path.isfile(pidfile):
+        pid = open(pidfile, 'r').readline().strip()
+    def start():
+        if os.path.isfile(pidfile):
+            if os.system('ps -p %s > /dev/null' % pid):
+                os.remove(pidfile)
+            else:
+                print('Server is already running!')
+                return
+        path = dirname(abspath(__file__))
+        os.chdir(path)
+        print('Currently on %s' % os.getcwd())
+        dir = os.path.basename(dirname(__file__))
+        os.system('gunicorn -c %s/%s %s.wsgi' % (dir, os.path.basename(__file__), dir))
+
+    def stop():
+        if os.path.isfile(pidfile):
+            if not os.system('ps -p %s > /dev/null' % pid):
+                print('Stopping server')
+                os.system('sudo -9 %s' % pid)
+            else:
+                print('Server not running')
+            os.remove(pidfile)
+        else:
+            print('No PID file found')
+
+    def restart():
+        from time import sleep
+        stop()
+        sleep(3)
+        start()
+
+    import sys
+    if len(sys.argv) == 2:
+        if sys.argv[1] == 'start':
+            start()
+        elif sys.argv[1] == 'stop':
+            stop()
+        elif sys.argv[1] == 'restart':
+            restart()
+        else:
+            print('Invalid command')
+    else:
+        print('Usage: { start | stop | restart }')
