438a1e15de35cbe88888c5cb15978dfd9aae6c58
mhaii <chess4G63>
2016-01-18 05:45:45 +0700

    api for switch between languages

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.utils.translation import LANGUAGE_SESSION_KEY
+
 
 # Create your views here.
+def switch_lang(request):
+    lang = request.session.get(LANGUAGE_SESSION_KEY, 'en')
+    request.session[LANGUAGE_SESSION_KEY] = 'en' if lang == 'th' else 'th'
+    return redirect(request.META.get('HTTP_REFERER', '/'))
