89f333eb8a5c2a8d7a1a2919076a3d85e5d8cf2d
mhaii <chess4G63>
2016-01-18 06:17:45 +0700

    change to mysql as default db when not debug

--- bedManagement/settings.py
+++ bedManagement/settings.py
@@ -91,7 +91,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
-    'production': {
+}
+if not DEBUG:
+    DATABASES['default'] = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bedmanagement',
         'USER': 'root',
@@ -99,7 +101,6 @@ DATABASES = {
         'HOST': '127.0.0.1',                        # localhost via TCP
         'PORT': '',                                 # default
     }
-}
 
 
 # Password validation
