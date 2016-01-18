86907a15d46fbe87a1459b7d5dcc7e053f777c6c
mhaii <chess4G63>
2016-01-19 03:20:12 +0700

    add __str__ to models

--- dashboard/models.py
+++ dashboard/models.py
@@ -6,15 +6,24 @@ from django.db.models import ForeignKey
 class Ward(Model):
     name = CharField('Name of the Ward', max_length=30, default='Olympus')
 
+    def __str__(self):
+        return str(self.name)
+
 
 class Patient(Model):
-    hn = PositiveIntegerField(unique=True, primary_key=True, editable=False)
+    hn = PositiveIntegerField(unique=True, primary_key=True)
     first_name = CharField(max_length=20)
     last_name = CharField(max_length=20)
+
+    def __str__(self):
+        return '[{0}] {1} {2}'.format(*map(str, [self.hn, self.first_name, self.last_name]))
 
 
 class Doctor(Model):
     name = CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.name)
 
 
 class User(Model):
@@ -31,6 +40,9 @@ class User(Model):
     role = SmallIntegerField('Role of the User', choices=enum_role, default=0)
     ward = ForeignKey(Ward, blank=True)
 
+    def __str__(self):
+        return '[{0}] {1}'.format(*map(str, [self.role, self.name]))
+
 
 class Room(Model):
     enum_status = [
@@ -41,12 +53,15 @@ class Room(Model):
         ('AVI', 'Available'),
     ]
 
-    room_number = CharField(max_length=7, editable=False)
+    room_number = CharField(max_length=7)
     status = CharField(max_length=3, choices=enum_status, default='DIS')
     phone = CharField(max_length=10)
     ward = ForeignKey(Ward)
     # not really sure about this
     type = CharField(max_length=20)
+
+    def __str__(self):
+        return '[{0}] {1} {2}'.format(*map(str, [self.ward, self.room_number, self.phone]))
 
 
 class Admit(Model):
@@ -60,7 +75,9 @@ class Admit(Model):
     patient = ForeignKey(Patient)
     doctor = ForeignKey(Doctor)
     status = CharField(max_length=3, choices=enum_status, default='QUE')
-    edd = PositiveSmallIntegerField('Estimated duration until discharged', default=-1)
+    edd = PositiveSmallIntegerField('Estimated day(s) to discharged', default=-1)
     symptom = CharField(max_length=30)
     admit_date = DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return str(self.patient)
