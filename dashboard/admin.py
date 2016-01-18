34bf3cd5f5950ba9500fe5e664394ec81a0bbc96
mhaii <bloo20xd@gmail.com>
2016-01-19 02:49:13 +0700

    add base models and their admin pages

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,40 @@
+from django.contrib.admin import ModelAdmin
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(ModelAdmin):
+    list_display = ['name', 'role', 'ward']
+    list_filter = ['role', 'ward']
+
+
+@admin.register(Ward)
+class WardAdmin(ModelAdmin):
+    list_filter = ['name']
+
+
+@admin.register(Room)
+class RoomAdmin(ModelAdmin):
+    list_display = ['room_number', 'ward', 'status', 'phone']
+    list_filter = ['ward', 'status']
+
+
+@admin.register(Admit)
+class AdmitAdmin(ModelAdmin):
+    list_display = ['patient', 'doctor', 'status', 'edd', 'symptom', 'admit_date']
+    list_filter = ['doctor', 'status', 'edd']
+    list_display_links = ['patient', 'doctor']
+
+
+@admin.register(Patient)
+class RoomAdmin(ModelAdmin):
+    list_display = ['hn', 'first_name', 'last_name']
+
+
+@admin.register(Doctor)
+class DoctorAdmin(ModelAdmin):
+    list_filter = ['name']
+
+
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,65 @@
-from django.db import models
+from django.db.models import Model, CharField, DateTimeField, PositiveIntegerField, PositiveSmallIntegerField, SmallIntegerField
+from django.db.models import ForeignKey
+
 
 # Create your models here.
+class User(Model):
+    enum_role = [
+        (-2, 'Cashier'),
+        (-1, 'Nurse Assistance'),
+        (0, 'Nobody'),
+        (1, 'Nurse'),
+        (2, 'Admission'),
+        (3, 'Administrator'),
+    ]
+
+    name = CharField('Name of the User', max_length=50, default='Saitama-san')
+    role = SmallIntegerField('Role of the User', choices=enum_role, default=0)
+    ward = ForeignKey(Ward, blank=True)
+
+
+class Ward(Model):
+    name = CharField('Name of the Ward', max_length=30, default='Olympus')
+
+
+class Room(Model):
+    enum_status = [
+        ('DIS', 'Disabled'),
+        ('RES', 'Reserved'),
+        ('OCC', 'Occupied'),
+        ('AVS', 'Available Soon'),
+        ('AVI', 'Available'),
+    ]
+
+    room_number = CharField(max_length=7, editable=False)
+    status = CharField(max_length=3, choices=enum_status, default='DIS')
+    phone = CharField(max_length=10)
+    ward = ForeignKey(Ward)
+    # not really sure about this
+    type = CharField(max_length=20)
+
+
+class Admit(Model):
+    enum_status = [
+        ('DIS', 'Discharged'),
+        ('PRE', 'Pre-discharged'),
+        ('CUR', 'Currently admit'),
+        ('QUE', 'In Queue'),
+    ]
+
+    patient = ForeignKey(Patient)
+    doctor = ForeignKey(Doctor)
+    status = CharField(max_length=3, choices=enum_status, default='QUE')
+    edd = PositiveSmallIntegerField('Estimated duration until discharged', default=-1)
+    symptom = CharField(max_length=30)
+    admit_date = DateTimeField(auto_now_add=True)
+
+
+class Patient(Model):
+    hn = PositiveIntegerField(unique=True, primary_key=True, editable=False)
+    first_name = CharField(max_length=20)
+    last_name = CharField(max_length=20)
+
+
+class Doctor(Model):
+    name = CharField(max_length=50)
