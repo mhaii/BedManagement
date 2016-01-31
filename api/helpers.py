6a532a3bfbce3a3426756149c47919440a6486ef
mhaii <chess4G63>
2016-01-31 21:22:53 +0700

    properly dump json response

--- api/helpers.py
+++ api/helpers.py
@@ -16,10 +16,12 @@ class BetterEncoder(json.JSONEncoder):
 def JsonErrorResponse(error):
     if not isinstance(error, str):
         error = str(error)
-    return json.dumps({'error': error})
+    return HttpResponse('{"error": "%s"}' % error, content_type='application/json')
 
 
 def JsonResponse(jsonstring):
+    if isinstance(jsonstring, QuerySet):
+        jsonstring = list(jsonstring)
     if not isinstance(jsonstring, str):
-        jsonstring = json.dumps(jsonstring)
+        jsonstring = json.dumps(jsonstring, cls=BetterEncoder)
     return HttpResponse(jsonstring, content_type='application/json')
