35a525abc00b1097c4f9b6314a71a8f8c002ba56
mhaii <chess4G63>
2016-02-06 18:28:26 +0700

    add Room update signal handler

--- dashboard/signals.py
+++ dashboard/signals.py
@@ -12,3 +12,23 @@ def handle_room_save(sender, instance, created, **kwargs):
         instance.save()
     else:
         print('room %s is updated' % instance)
+
+
+@receiver(pre_save, sender=Admit)
+def handle_room_admits(sender, instance, **kwargs):
+    current = Admit.objects.get(id=instance.id)
+
+    if instance.status != current.status:
+        if instance.status == 3:
+            instance.room.status = 'AS'
+            instance.room.save()
+        elif instance.status == 4:
+            instance.room = None
+
+    if instance.room != current.room:
+        if current.room is not None:
+            current.room.status = 'AV'
+            current.room.save()
+        if instance.room is not None:
+            instance.room.status = 'OC'
+            instance.room.save()
