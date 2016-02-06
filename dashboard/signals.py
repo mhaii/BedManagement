6a94012333dead0e64d3ee53b0859dd8045bbd53
mhaii <chess4G63>
2016-02-06 21:55:59 +0700

    fix model DOE from signal

--- dashboard/signals.py
+++ dashboard/signals.py
@@ -16,19 +16,22 @@ def handle_room_save(sender, instance, created, **kwargs):
 
 @receiver(pre_save, sender=Admit)
 def handle_room_admits(sender, instance, **kwargs):
-    current = Admit.objects.get(id=instance.id)
+    try:
+        current = Admit.objects.get(id=instance.id)
 
-    if instance.status != current.status:
-        if instance.status == 3:
-            instance.room.status = 'AS'
-            instance.room.save()
-        elif instance.status == 4:
-            instance.room = None
+        if instance.status != current.status:
+            if instance.status == 3:
+                instance.room.status = 'AS'
+                instance.room.save()
+            elif instance.status == 4:
+                instance.room = None
 
-    if instance.room != current.room:
-        if current.room is not None:
-            current.room.status = 'AV'
-            current.room.save()
-        if instance.room is not None:
-            instance.room.status = 'OC'
-            instance.room.save()
+        if instance.room != current.room:
+            if current.room is not None:
+                current.room.status = 'AV'
+                current.room.save()
+            if instance.room is not None:
+                instance.room.status = 'OC'
+                instance.room.save()
+    except Admit.DoesNotExist:
+        pass
