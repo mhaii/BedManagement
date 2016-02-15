78b1a589ba35f3e6d8b1eec9ca4d9b41b5533a33
mhaii <chess4G63>
2016-02-15 12:00:19 +0700

    group routes for apis

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root 'application#index'
-  get 'wards/rooms' => 'wards#rooms_all'
-  get 'wards/:id/rooms' => 'wards#rooms'
-  resources :rooms
-  resources :admits
-  resources :wards
+
+  scope 'resources' do
+    get 'wards/rooms' => 'wards#rooms_all'
+    get 'wards/:id/rooms' => 'wards#rooms'
+    resources :rooms
+    resources :admits
+    resources :wards
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
