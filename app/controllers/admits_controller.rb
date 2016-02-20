7607b9a8fa3fea7f396cf5f9e40aba89463869ef
mhaii <chess4G63>
2016-02-21 03:27:09 +0700

    finish {update room} on `Admit` save

--- app/controllers/admits_controller.rb
+++ app/controllers/admits_controller.rb
@@ -9,57 +9,35 @@ class AdmitsController < ApplicationController
   end
 
   def today
-    @admits = Admit.where status: 1, admitted_date: Date.today.beginning_of_day..Date.today.end_of_day
+    @admits = Admit.where status: 1,        admitted_date: Date.today.beginning_of_day..Date.today.end_of_day
     render 'detail'
   end
 
   def out_soon
-    @admits = Admit.where status: [2, 3], edd: 1.month.ago..3.days.from_now
+    @admits = Admit.where status: [2, 3],   edd: 1.month.ago..3.days.from_now
     render 'detail'
   end
 
   def create
     @admit = Admit.new(get_request_body)
-    respond_to do |format|
-      if @admit.save
-        format.json { render :show, status: :created, location: @admit }
-      else
-        format.json { render json: @admit.errors, status: :unprocessable_entity }
-      end
+    if @admit.save
+      render :show, status: :created, location: @admit
+    else
+      render json: @admit.errors, status: :unprocessable_entity
     end
   end
 
   def update
-    update_params = get_request_body
-    if @admit.status != update_params.status
-      if update_params.status == 3
-        @admit.room.update status: :availableSoon
-      elsif update_params.status == 4
-        update_params.room_id = nil
-      end
-    end
-    if update_params.room_id
-      if @admit.room
-        @admit.room.update status: :available
-      end
-      Room.find(update_params.room_id).update status: :occupied
+    if @admit.update(get_request_body)
+      render :show, status: :ok, location: @admit
     else
-      @admit.room.update status: :available
-    end
-    respond_to do |format|
-      if @admit.update(update_params)
-        format.json { render :show, status: :ok, location: @admit }
-      else
-        format.json { render json: @admit.errors, status: :unprocessable_entity }
-      end
+      render json: @admit.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
     @admit.destroy
-    respond_to do |format|
-      format.json { head :no_content }
-    end
+    head :no_content
   end
 
   private
--- app/models/admit.rb
+++ app/models/admit.rb
@@ -1,6 +1,23 @@
 class Admit < ActiveRecord::Base
+  before_save :update_room
+
   enum status: [:pending, :confirmed, :currentlyAdmit, :preDischarged, :discharged]
   belongs_to  :patient
   belongs_to  :room
   belongs_to  :doctor
+
+  private
+    def update_room
+      case self.status
+        when 'preDischarged'
+          self.room.update status: :availableSoon
+        when 'discharged'
+          self.room = nil
+      end if status_changed?
+
+      if room_id_changed?
+        Room.find_by_id(self.room_id_was).update status: :available unless self.room_id_was.nil?
+        Room.find_by_id(self.room_id).update status: :occupied      unless self.room_id.nil?
+      end
+    end
 end
