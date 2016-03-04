25b40f467289db7b2be8dbc36bea1a2dfe251d48
Boss <nutchanon.9@gmail.com>
2016-03-04 15:15:07 +0700

    add websocket

--- app/models/check_out_step.rb
+++ app/models/check_out_step.rb
@@ -1,4 +1,10 @@
 class CheckOutStep < ActiveRecord::Base
   belongs_to :admit
   before_create { self.time_started = DateTime.now }
+  after_save :trigger_update_event
+
+  private
+    def trigger_update_event
+      WebsocketRails[:admits].trigger 'check_out', self
+    end
 end
