9063218f190a86b3e5c108d2cf8ed5bed8dc3e7c
mhaii <ppuri.yc@gmail.com>
2016-03-09 23:14:42 +0700

    trigger socket update for room disabled

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,12 @@ class Room < ActiveRecord::Base
   enum status: {disabled: -2, reserved: -1, available: 0, availableSoon: 1, occupied: 2}
   belongs_to  :ward
   has_one     :admit
+
+  before_save :check_status
+
+  def check_status
+    if [status_was, status].include? 'disabled'
+      WebsocketRails[:admits].trigger 'updated', self
+    end
+  end
 end
