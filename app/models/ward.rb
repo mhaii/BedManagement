985296c99dbd3d6711b2796671d4ef44298b324d
mhaii <ppuri.yc@gmail.com>
2016-03-14 18:18:39 +0700

    add ward model spec

--- app/models/ward.rb
+++ app/models/ward.rb
@@ -2,4 +2,6 @@ class Ward < ActiveRecord::Base
   has_many :users
   has_many :rooms, -> { order :number }
   has_and_belongs_to_many :departments
+
+  validates_presence_of :name
 end
---
+++ spec/models/ward_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Ward, type: :model do
+  before do
+    @ward = Ward.create({name: 'WardName'})
+  end
+
+  it 'must not allow record without necessary attribute' do
+    expect(Ward.create.valid?).to be_falsey
+    expect(Ward.create({name: 'Name'}).valid?).to be_truthy
+  end
+
+  it 'must return rooms sorted by room number no matter the order of their ids' do
+    Room.create({ward: @ward, number: 215})
+    Room.create({ward: @ward, number: 207})
+    Room.create({ward: @ward, number: 222})
+    Room.create({ward: @ward, number: 201})
+
+    rooms = @ward.rooms.map &:number
+    expect(rooms[0]).to eq '201'
+    expect(rooms[1]).to eq '207'
+    expect(rooms[2]).to eq '215'
+    expect(rooms[3]).to eq '222'
+  end
+end
