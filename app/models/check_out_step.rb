e07fa3120dd12433aa15cb151977c819ee80026f
mhaii <chess4G63>
2016-03-23 01:06:02 +0700

    auto start last checkout step if all previous steps are completed

--- app/models/check_out_step.rb
+++ app/models/check_out_step.rb
@@ -12,6 +12,14 @@ class CheckOutStep < ActiveRecord::Base
   private
     def trigger_update_event
       WebsocketRails[:admits].trigger 'check_out', admit.check_out_steps
+
+      # auto start last one if all previous steps are completed
+      admit.check_out_steps.each do |step|
+        step.update time_started: DateTime.now if step.CHECK_OUT_PATIENT_LEAVE? and !step.time_started
+        break unless step.time_ended
+      end
+
+      # log to db
       self.changes.each do |attr, change|
         Log.create({user: User.current, model: self.class.name, attr: attr, change: "#{change[0]} -> #{change[1]}",
                     target_id: self.id, logged_at: DateTime.now})
