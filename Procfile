0987d6bc9075828c9e23f96bc897f46b34520851
mhaii <chess4G63>
2016-03-25 04:54:06 +0700

    dump puma and switch to thin

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: bundle exec puma -C config/puma.rb
+web: bundle exec thin start -p $PORT -e $RACK_ENV
+puma: bundle exec puma -C config/puma.rb
--- config/initializers/eventmachine.rb
+++
@@ -1,2 +0,0 @@
-# Allow websocket to be use on event-base webserver other than `thin`
-Thread.new { EventMachine.run } unless EventMachine.reactor_running? && EventMachine.reactor_thread.alive?
